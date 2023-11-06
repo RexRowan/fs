cc3fbc75956bae5426bb32d3b94fdcc622e99d97
MeanMachineRex 章魚貓 <RexRowan@users.noreply.github.com>
2023-11-06 14:54:10 +0000

    1.5

--- part1/part1b/part1b/src/App.jsx
+++ part1/part1b/part1b/src/App.jsx
@@ -1,28 +1,30 @@
 const App = () => {
-  const course = 'Half Stack application development';
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    },
-  ];
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   )
-};
+}
 
 const Header = ({ course }) => {
   return (
@@ -35,9 +37,9 @@ const Header = ({ course }) => {
 const Content = ({ parts }) => {
   return (
     <div>
-      {parts.map((item, index) => (
+      {parts.map((part, index) => (
         <p key={index}>
-          {item.name} {item.exercises}
+          {part.name} {part.exercises}
         </p>
       ))}
     </div>
@@ -45,7 +47,7 @@ const Content = ({ parts }) => {
 };
 
 const Total = ({ parts }) => {
-  const total = parts.reduce((sum, item) => sum + item.exercises, 0);
+  const total = parts.reduce((sum, part) => sum + part.exercises, 0);
   return (
     <div>
       <p>Number of exercises {total}</p>
