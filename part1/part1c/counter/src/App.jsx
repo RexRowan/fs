7e8f0b832a958d91e83d46062c64868af0147e0a
MeanMachineRex 章魚貓 <RexRowan@users.noreply.github.com>
2023-11-07 11:28:17 +0000

    refactor

--- part1/part1c/counter/src/App.jsx
+++ part1/part1c/counter/src/App.jsx
@@ -1,18 +1,42 @@
 import { useState } from 'react'
+
 const App = () => {
-  const [ counter, setCounter ] = useState(0)
+
+  const Display = ({ counter }) => <div>{counter}</div>
+
+  const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
+
+  const [counter, setCounter] = useState(0)
+
+  console.log('rendering with counter value', counter)
+
+  const increaseByOne = () => {
+
+    console.log('increasing, value before', counter)
+    setCounter(counter + 1)
+  }
+
+  const decreaseByOne = () => { 
+
+    console.log('decreasing, value before', counter)
+    setCounter(counter - 1)
+  }
+
+  const setToZero = () => {
+
+    console.log('resetting to zero, value before', counter)
+    setCounter(0)
+  }
+
 
   return (
     <div>
-      <div>{counter}</div>
-      <button onClick={() => setCounter(counter + 1)}>
-        plus
-      </button>
-      <button onClick={() => setCounter(0)}> 
-        zero
-      </button>
+      <Display counter={counter} />
+      <Button onClick={increaseByOne} text="plus" />
+      <Button onClick={setToZero} text="zero" />
+      <Button onClick={decreaseByOne} text="minus" />
     </div>
   )
-}
+} 
 
 export default App
