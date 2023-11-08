b8cac0b7a8c1cc85fe3fcc612b07b7d587c51771
MeanMachineRex 章魚貓 <RexRowan@users.noreply.github.com>
2023-11-08 13:32:04 +0000

    1.12

--- part1/part1d/anecdotes/src/App.jsx
+++ part1/part1d/anecdotes/src/App.jsx
@@ -12,13 +12,21 @@ const App = () => {
     'The only way to go fast, is to go well.'
   ]
    
-  const [selected, setSelected] = useState(0)
+  const getRandomIndex = () => Math.floor(Math.random() * anecdotes.length);
+   
+  const [selected, setSelected] = useState(getRandomIndex());
+
+  const nextAnecdote = () => {
+    setSelected(getRandomIndex());
+  }
 
   return (
     <div>
       {anecdotes[selected]}
+      <br/>
+      <br/>
+      <button onClick={nextAnecdote}>Next Anecdote</button>
     </div>
   )
 }
-
 export default App
