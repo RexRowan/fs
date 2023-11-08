380551b21119185b3138b7e83d9b63b03acd24d1
MeanMachineRex 章魚貓 <RexRowan@users.noreply.github.com>
2023-11-08 10:20:54 +0000

    step1

--- part1/part1d/unicafe/src/App.jsx
+++ part1/part1d/unicafe/src/App.jsx
@@ -1,14 +1,51 @@
 import { useState } from 'react'
 
+const History = (props) => {
+    
+    <div>
+      button press history: {props.allClicks.join(' ')}
+    </div>
+  
+}
+
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
+
 const App = () => {
-  // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [allClicks, setAll] = useState([]) 
+
+  const handleGoodClick = () => {
+    setAll(allClicks.concat('G'))
+    setGood(good + 1)
+  }
+
+  const handleNeutralClick = () => {
+    setAll(allClicks.concat('N'))
+    setNeutral(neutral + 1)
+  }
+
+  const handleBadClick = () => {
+    setAll(allClicks.concat('B'))
+    setBad(bad + 1)
+  }
 
   return (
     <div>
-      code here
+      <h1>give feedback</h1>
+      {good}
+      <Button handleClick={handleGoodClick} text='good' />
+      {neutral}
+      <Button handleClick={handleNeutralClick} text='nautral' />
+      {bad}
+      <Button handleClick={handleBadClick} text='bad' />
+      <History allClicks={allClicks} />
+      <h1>statistics</h1>
     </div>
   )
 }
