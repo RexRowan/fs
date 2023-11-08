66d3cecb857ca361e67a7f13adb852ac81479c26
MeanMachineRex 章魚貓 <RexRowan@users.noreply.github.com>
2023-11-08 11:39:09 +0000

    1.1 Step6

--- part1/part1d/unicafe/src/App.jsx
+++ part1/part1d/unicafe/src/App.jsx
@@ -7,7 +7,10 @@ const Button = ({ handleClick, text }) => (
 )
 
 const StatisticLine = ({ text, value }) => (
-  <p>{text}: {value}</p>
+  <tr>
+    <td>{text}</td>
+    <td>{value}</td>
+  </tr>
 );
 
 
@@ -28,12 +31,16 @@ const Statistics = ({good, neutral, bad}) => {
   return (
     <div>
       <h1>statistics</h1>
+      <table>
+        <tbody>
       <StatisticLine text="good" value={good} />
       <StatisticLine text="neutral" value={neutral} />
       <StatisticLine text="bad" value={bad} />
       <StatisticLine text="all" value={total} />
       <StatisticLine text="average" value={average.toFixed(2)} />
       <StatisticLine text="positive" value={`${positivePercentage.toFixed(2)}%`} />
+        </tbody>
+      </table>
     </div>
   );
 };
