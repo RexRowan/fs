74e3d3a3b519b3ae7581a2b0e45a8fbece9c5d1d
MeanMachineRex 章魚貓 <RexRowan@users.noreply.github.com>
2023-11-11 18:00:56 +0000

    2.5

--- part2/part2.1/courseinfo/src/App.jsx
+++ part2/part2.1/courseinfo/src/App.jsx
@@ -2,43 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Course from './Course.jsx';
 
-const Part = ({ part }) => {
-  return (
-    <p>
-      {part.name} {part.exercises}
-    </p>
-  )
-}
-
-const Content = ({ parts }) => {
-  return (
-    <div>
-      {parts.map(part => <Part key={part.id} part={part} />)}
-    </div>
-  )
-}
-
-const Header = ({ name }) => {
-  return (
-    <h2>{name}</h2>
-  )
-}
-
-const Course = ({ course }) => {
-  return (
-    <div>
-      <Header name={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-    </div>
-  )
-}
-
-const Total = ({ parts }) => {
-  const total = parts.reduce((sum, part) => sum + part.exercises, 0);
-  return <p><b>Total of {total} exercises</b></p>;
-};
 
 const App = () => {
   const courses = [
