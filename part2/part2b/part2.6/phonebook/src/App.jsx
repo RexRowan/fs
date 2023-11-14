1342238d710bcb597e8af3138d9fab276426975d
MeanMachineRex 章魚貓 <RexRowan@users.noreply.github.com>
2023-11-14 15:19:03 +0000

    add names

--- part2/part2b/part2.6/phonebook/src/App.jsx
+++ part2/part2b/part2.6/phonebook/src/App.jsx
@@ -7,14 +7,33 @@ const App = () => {
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas' }
   ]) 
+
   const [newName, setNewName] = useState('')
+
+  const addPerson = (event) => {
+    event.preventDefault(); 
+
+    const personObject = {
+      name: newName
+    };
+
+// Check if the new name already exists in the array
+if (persons.some(person => person.name === newName)) {
+  alert(`${newName} is already added to the phonebook.`);
+} else {
+  setPersons(persons.concat(personObject));
+  setNewName('');
+}
+};
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addPerson}>
         <div>
-          name: <input />
+          name: <input value={newName}
+            onChange={(e) => setNewName(e.target.value)}
+          />
         </div>
         <div>
           <button type="submit">add</button>
