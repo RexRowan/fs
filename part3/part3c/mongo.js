8dea932bc7381b45c1ef68afbbd55e456da8339d
MeanMachineRex <titaniumdavinci@gmail.com>
2026-09-25 12:29:26 +0100

    phonebook working

--- part3/part3c/mongo.js
+++ part3/part3c/mongo.js
@@ -7,38 +7,41 @@ if (process.argv.length < 3) {
 
 const password = process.argv[2]
 
-const url = `mongodb+srv://titaniumdavinci_db_user:${password}@cluster0.adeveta.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+const url =
+  `mongodb+srv://titaniumdavinci_db_user:${password}@cluster0.adeveta.mongodb.net/phonebookApp?retryWrites=true&w=majority&appName=Cluster0`
 
-mongoose.set('strictQuery',false)
-
-require('node:dns').setServers(['1.1.1.1', '8.8.8.8']);
+mongoose.set('strictQuery', false)
 
 mongoose.connect(url, { family: 4 })
 
-const noteSchema = new mongoose.Schema({
-  content: String,
-  important: Boolean,
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
 })
 
-const Note = mongoose.model('Note', noteSchema)
+const Person = mongoose.model('Person', personSchema)
 
-/*
-const note = new Note({
-  content: 'HTML is easy',
-  important: true,
-})
-  */
-
-Note.find({}).then(result => {
-  result.forEach(note => {
-    console.log(note)
+if (process.argv.length === 3) {
+  // only password given -> list all entries
+  Person.find({}).then(result => {
+    console.log('phonebook:')
+    result.forEach(person => {
+      console.log(`${person.name} ${person.number}`)
+    })
+    mongoose.connection.close()
   })
-  mongoose.connection.close()
-})
+} else if (process.argv.length === 5) {
+  // password, name, number given -> add new entry
+  const person = new Person({
+    name: process.argv[3],
+    number: process.argv[4],
+  })
 
-/*
-note.save().then(result => {
-  console.log('note saved!')
-  mongoose.connection.close()
-})
-  */
+  person.save().then(() => {
+    console.log(`added ${person.name} number ${person.number} to phonebook`)
+    mongoose.connection.close()
+  })
+} else {
+  console.log('invalid number of arguments')
+  process.exit(1)
+}
